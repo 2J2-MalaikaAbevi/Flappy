4ec4bbde322a18f607b218b894b61625a99ddad3
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2024-02-17 10:23:37 -0500

    Ajout de la détection de touches
    Script pour le déplacement de Flappy complété

--- Assets/Scripts/ControleFlappy.cs
+++ Assets/Scripts/ControleFlappy.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class ControleFlappy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Déclarations des variables (publiques)
+    public float vitesseX;    //La vitesse de déplacement horizontal de Flappy
+    public float vitesseY;    //La vitesse de déplacement vertical de Flappy
 
-    // Update is called once per frame
+    // Fonction qui gère les déplacements et le saut de Flappy à l'aide des touches Right (ou A), Left (ou D) et Up (ou W).
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            vitesseX -= 1;
+        } 
+        else if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            vitesseX += 1;
+        }
+        else 
+        { 
+            vitesseX = GetComponent<Rigidbody2D>().velocity.x;
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            vitesseY += 1;
+        }
+        else
+        {
+            vitesseY = GetComponent<Rigidbody2D>().velocity.y;
+        }
+
+        // On ajuste la vélocité du personnage en lui attribuant la valeur de la variable locale
+        GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
     }
 }
