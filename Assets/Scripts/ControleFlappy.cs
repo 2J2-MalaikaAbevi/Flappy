dd23193842f2fca3e62a334119a3541948374d00
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2024-02-19 12:47:58 -0500

    Ajustement du déplacement de FLappy
    Ajustements des paramètres du Rigidbody 2D et ajustements des vitesses en X et Y de Flappy

--- Assets/Scripts/ControleFlappy.cs
+++ Assets/Scripts/ControleFlappy.cs
@@ -11,13 +11,14 @@ public class ControleFlappy : MonoBehaviour
     // Fonction qui gère les déplacements et le saut de Flappy à l'aide des touches Right (ou A), Left (ou D) et Up (ou W).
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            vitesseX -= 1;
+            vitesseX = -0.5f;
         } 
-        else if(Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            vitesseX += 1;
+            vitesseX = 0.5f;
+
         }
         else 
         { 
@@ -26,7 +27,7 @@ public class ControleFlappy : MonoBehaviour
 
         if(Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            vitesseY += 1;
+            vitesseY = 3.5f;
         }
         else
         {
