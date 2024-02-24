31bb3f4642d06605aeb5e1942eb1d692e9ef4d32
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2024-02-24 00:54:39 -0500

    Version Finale de Flappy
    Ajout de commentaires et finalisation du code

--- Assets/Scripts/ControleFlappy.cs
+++ Assets/Scripts/ControleFlappy.cs
@@ -6,7 +6,7 @@ using UnityEngine;
    Gestion des déplacements horizontaux et du saut de Flappy à l'aide des touches : a ou flèche gauche, d ou flèche haut, w ou flèche droite.
    Gestion des détections de collisions entre le personnage et les objets du jeu.
    Par : Malaïka Abevi
-   Dernière modification : 20/02/2024
+   Dernière modification : 24/02/2024
 */
 
 public class ControleFlappy : MonoBehaviour
@@ -16,10 +16,11 @@ public class ControleFlappy : MonoBehaviour
     public float vitesseY;    //La vitesse de déplacement vertical de Flappy
 
     public Sprite flappyMalade; //L'image de Flappy malade
+    public Sprite flappyBase; //L'image de Flappy de base
 
-    public GameObject laPieceOr;
-    public GameObject lePackVie;
-    public GameObject leChampignon;
+    public GameObject laPieceOr;  //Le game object de la pièce d'or
+    public GameObject lePackVie;  //Le game object du pack de vie
+    public GameObject leChampignon;  //Le game object du champignon
 
     // Fonction qui gère les déplacements et le saut de Flappy à l'aide des touches Right (ou A), Left (ou D) et Up (ou W).
     void Update()
@@ -55,59 +56,94 @@ public class ControleFlappy : MonoBehaviour
         GetComponent<Rigidbody2D>().velocity = new Vector2(vitesseX, vitesseY);
     }
 
+    //Fonction pour la détection des collisions avec les objets et Flappy, puis des actions qui se produiront suite à une collision
     void OnCollisionEnter2D(Collision2D infoCollision)
     {
+        //On vérifie si une collision est détectée avec une colonne
         if(infoCollision.gameObject.name == "ColonneHaut" || infoCollision.gameObject.name == "ColonneBas")
         {
+            //On donne alors une image de Flappy malade qui remplacera l'image de base de Flappy 
             GetComponent<SpriteRenderer>().sprite = flappyMalade;
         }
 
+        //On vérifie si un collision est détectée avec la pièce
         if(infoCollision.gameObject.name == "PieceOr")
-        {
+        {    
+            /*print(infoCollision.gameObject.name); Test pour vérifier la détection*/
+            //On désactive alors la pièce d'or (le game object)
             infoCollision.gameObject.SetActive(false);
 
-            float valeurAleatoireY = Random.Range(-3, 3);
+            //Une valeur aléatoire est choisit entre -2 et 2 pour repositionner la pièce d'or sur la verticale
+            float valeurAleatoireY = Random.Range(-2f, 2f);
 
-            infoCollision.gameObject.transform.position = new Vector2(6, valeurAleatoireY);
+            //On donne alors de nouvelles coordonnées avec une position X fixe et la valeur aléatoire qui a été générée
+            infoCollision.gameObject.transform.localPosition = new Vector2(-4, valeurAleatoireY);
 
-            Invoke("ReactiverPieceOr", 5f);
+            //Puis, on appelle une fonction qui servira a faire réapparaitre la pièce d'or au bout de 6 secondes, le temps que la colonne sorte du décor
+            Invoke("ReactiverPieceOr", 6f);
         }
 
-        if(infoCollision.gameObject.name == "PackVie")
+        //On vérifie si un collision est détectée avec le pack de vie
+        if (infoCollision.gameObject.name == "PackVie")
         {
+            /*On donne (ou redonne) alors une image de Flappy de base qui remplacera l'image de Flappy malade 
+             (ou elle demeure la même si Flappy n'était pas malade puisque c'est la même image)*/
+            GetComponent<SpriteRenderer>().sprite = flappyBase;
+
+            //On désactive alors le pack de vie (le game object)
             infoCollision.gameObject.SetActive(false);
 
-            float valeurAleatoireY = Random.Range(-3, 3);
+            //Une valeur aléatoire est choisit entre -2 et 2 pour repositionner le pack de vie sur la verticale
+            float valeurAleatoireY = Random.Range(-2f, 2f);
 
-            infoCollision.gameObject.transform.position = new Vector2(6, valeurAleatoireY);
+            //On donne alors de nouvelles coordonnées avec une position X fixe et la valeur aléatoire qui a été générée
+            infoCollision.gameObject.transform.localPosition = new Vector2(-4, valeurAleatoireY);
 
-            Invoke("ReactiverPackVie", 5f);
+            //Puis, on appelle une fonction qui servira a faire réapparaitre le pack de vie au bout de 6 secondes, le temps que la colonne sorte du décor
+            Invoke("ReactiverPackVie", 7f);
         }
 
+        //On vérifie si un collision est détectée avec le champignon
         if (infoCollision.gameObject.name == "Champignon")
         {
+            //On grossit la taille de Flappy de 50%
+            transform.localScale *= 1.5f;
+
+            //On désactive alors le champignon (le game object)
             infoCollision.gameObject.SetActive(false);
 
-            float valeurAleatoireY = Random.Range(-3, 3);
+            //Une valeur aléatoire est choisit entre -2 et 2 pour repositionner le pack de vie sur la verticale
+            float valeurAleatoireY = Random.Range(-2f, 2f);
 
-            infoCollision.gameObject.transform.position = new Vector2(6, valeurAleatoireY);
+            //On donne alors de nouvelles coordonnées avec une position X fixe et la valeur aléatoire qui a été générée
+            infoCollision.gameObject.transform.localPosition = new Vector2(-4, valeurAleatoireY);
 
-            Invoke("ReactiverChampignon", 5f);
+            //Puis, on appelle une fonction qui servira a faire réapparaitre le champignon au bout de 6 secondes, le temps que la colonne sorte du décor
+            Invoke("ReactiverChampignon", 6f);
         }
     }
 
+    //Fonction pour réactiver la pièce d'or (game object)
     void ReactiverPieceOr()
     {
+        //Réactivation de la pièce d'or
         laPieceOr.SetActive(true);
     }
 
+    //Fonction pour réactiver le pack de vie (game object)
     void ReactiverPackVie()
     {
+        //Réactivation du pack de vie
         lePackVie.SetActive(true);
     }
 
+    //Fonction pour réactiver le champignon (game object)
     void ReactiverChampignon()
     {
+        //Réactivation du champignon
         leChampignon.SetActive(true);
+
+        //On redonne la taille d'origine à Flappy en la diminuant de 50%
+        transform.localScale /= 1.5f;
     }
 }
--- Assets/Scripts/DeplacementObjets.cs
+++ Assets/Scripts/DeplacementObjets.cs
@@ -2,29 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Fonctionnement et utilité générale du script:
+   Gestion des déplacements horizontaux et des positions verticales des objets du jeu.
+   Par : Malaïka Abevi
+   Dernière modification : 23/02/2024
+*/
+
 public class DeplacementObjets : MonoBehaviour
 {
-    public float vitesse;
-    public float positionFin;
-    public float positionDebut;
-    public float deplacementAleatoire;
-    //
+    //Déclarations des variables (publiques)
+    public float vitesse; //La vitesse de l'objet
+    public float positionFin; //La position de fin de l'objet, sa limite horizontale qui l'a fera réapparaitre à sa position de début
+    public float positionDebut; //La position de début de l'objet, où elle réapparaitra après avoir atteint sa position de fin
+    public float deplacementAleatoire; //Valeur déterminant de position aléatoire verticale possible pour l'objet
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //Fonction qui gère le déplacement à l'horizontale et le repositionnement à la verticale
     void Update()
     {   
+        //On applique la valeur de la variable vitesse et on fait déplacer l'objet
         transform.Translate(vitesse, 0, 0);
 
+        //On détermine si la position horizontale correspond à la position de fin 
         if(transform.position.x < positionFin)
         {
+            //Alors, on crée une variable qui enregistrera une valeur aléatoire qui sera généré à partir de la valeur de la variable 'deplacementAleatoire'
             float valeurAleatoireY = Random.Range(-deplacementAleatoire, deplacementAleatoire);
 
+            //On attribue des nouvelles coordonnées de position. On ramène l'objet à sa position de début (en X) et on la repositionne avec la variable de valeur aléatoire (en Y)
             transform.position = new Vector2(positionDebut, valeurAleatoireY);
         }
     }
